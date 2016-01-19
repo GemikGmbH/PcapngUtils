fb5bdb10bf1dd2c9ba37758a67c45deb1b3e184d
r1pper <the.r1pper@hotmail.com>
2016-01-19 17:50:32 +0330

    changed reversal parts with faster versions

--- PcapngUtils/Extensions/ReverseByteOrderExtension.cs
+++ PcapngUtils/Extensions/ReverseByteOrderExtension.cs
@@ -1,79 +1,94 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace PcapngUtils.Extensions
 {    
     public static class ReverseByteOrderExtension
-    {          
-        public static UInt32 ReverseByteOrder(this UInt32 value,bool reverseByteOrder)
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UInt32 ReverseByteOrder(this UInt32 value, bool reverseByteOrder)
         {
             if (!reverseByteOrder)
                 return value;
-            else
-            {
-                byte[] bytes = BitConverter.GetBytes(value); 
-                Array.Reverse(bytes);
-                return BitConverter.ToUInt32(bytes, 0);
-            }
+
+            return (value << 24) |
+                   ((value << 8) & 0x00ff0000) |
+                   ((value >> 8) & 0x0000ff00) |
+                   ((value >> 24) & 0x000000ff);
+
+            //value = (value >> 16) | (value << 16);
+            //return ((value & 0xFF00FF00) >> 8) | ((value & 0x00FF00FF) << 8);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Int32 ReverseByteOrder(this Int32 value, bool reverseByteOrder)
         {
             if (!reverseByteOrder)
                 return value;
-            else
-            {
-                byte[] bytes = BitConverter.GetBytes(value);
-                Array.Reverse(bytes);
-                return BitConverter.ToInt32(bytes, 0);
-            }
+
+            return (value << 24) |
+                   ((value << 8) & 0x00ff0000) |
+                   ((value >> 8) & 0x0000ff00) |
+                   ((value >> 24) & 0x000000ff);
+
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static UInt16 ReverseByteOrder(this UInt16 value, bool reverseByteOrder)
         {
             if (!reverseByteOrder)
                 return value;
-            else
-            {
-                byte[] bytes = BitConverter.GetBytes(value);
-                Array.Reverse(bytes);
-                return BitConverter.ToUInt16(bytes, 0);
-            }
+
+            return (ushort)((value << 8) | ((value >> 8) & 0x00ff));
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Int16 ReverseByteOrder(this Int16 value, bool reverseByteOrder)
         {
             if (!reverseByteOrder)                
                 return value;
-            else
-            {
-                byte[] bytes = BitConverter.GetBytes(value);
-                Array.Reverse(bytes);
-                return BitConverter.ToInt16(bytes, 0);
-            }
+
+            return (short)((value << 8) | ((value >> 8) & 0x00ff));
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static UInt64 ReverseByteOrder(this UInt64 value, bool reverseByteOrder)
         {
             if (!reverseByteOrder)
                 return value;
-            else
-            {
-                byte[] bytes = BitConverter.GetBytes(value);
-                Array.Reverse(bytes);
-                return BitConverter.ToUInt64(bytes, 0);
-            }
+
+            return ((0x00000000000000FF) & (value >> 56)
+                    | (0x000000000000FF00) & (value >> 40)
+                    | (0x0000000000FF0000) & (value >> 24)
+                    | (0x00000000FF000000) & (value >> 8)
+                    | (0x000000FF00000000) & (value << 8)
+                    | (0x0000FF0000000000) & (value << 24)
+                    | (0x00FF000000000000) & (value << 40)
+                    | (0xFF00000000000000) & (value << 56));
+
+            //value = (value >> 32) | (value << 32);
+            //value = ((value & 0xFFFF0000FFFF0000) >> 16) | ((value & 0x0000FFFF0000FFFF) << 16);
+            //return ((value & 0xFF00FF00FF00FF00) >> 8) | ((value & 0x00FF00FF00FF00FF) << 8);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Int64 ReverseByteOrder(this Int64 value, bool reverseByteOrder)
         {
             if (!reverseByteOrder)
                 return value;
-            else
-            {
-                byte[] bytes = BitConverter.GetBytes(value);
-                Array.Reverse(bytes);
-                return BitConverter.ToInt64(bytes, 0);
-            }
+
+            return ((0x00000000000000FF) & (value >> 56)
+                    | (0x000000000000FF00) & (value >> 40)
+                    | (0x0000000000FF0000) & (value >> 24)
+                    | (0x00000000FF000000) & (value >> 8)
+                    | (0x000000FF00000000) & (value << 8)
+                    | (0x0000FF0000000000) & (value << 24)
+                    | (0x00FF000000000000) & (value << 40)
+                    | (long) ((0xFF00000000000000) & ((ulong) (value << 56))));
+
+            //value = (value >> 32) | (value << 32);
+            //value = ((value & 0xFFFF0000FFFF0000) >> 16) | ((value & 0x0000FFFF0000FFFF) << 16);
+            //return ((value & 0xFF00FF00FF00FF00) >> 8) | ((value & 0x00FF00FF00FF00FF) << 8);
         }
     }
 }
